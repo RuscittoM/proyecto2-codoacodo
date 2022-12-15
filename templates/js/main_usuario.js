4b007b5f57f4a3ba0bfb6194564db719b67194b8
micaela <Ruscittomicaela@gmail.com>
2022-12-15 12:59:36 -0300

    solucion carrousel

--- templates/js/main_usuario.js
+++ templates/js/main_usuario.js
@@ -49,7 +49,7 @@ document.write(sup)
 
 // carrousel
 
-var imagenes =['/img/principal2.png', '/img/principal.jpg' , '/img/images.jpg'],
+var imagenes =['templates/img/principal2.png', 'templates/img/principal.jpg' , 'templates/img/images.jpg'],
 cont=0;
 
 function carrousel (contenedor){
